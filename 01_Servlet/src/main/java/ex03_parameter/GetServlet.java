f8966f2e29e01ba2b82efff2b9ae2259fa05d1ca
gdu-dev <minkt78@naver.com>
2023-03-16 12:06:45 +0900

    2023-03-16

--- 01_Servlet/src/main/java/ex03_parameter/GetServlet.java
+++ 01_Servlet/src/main/java/ex03_parameter/GetServlet.java
@@ -12,10 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 public class GetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public GetServlet() {
-        super();
-    }
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		/*
--- 01_Servlet/src/main/java/ex04_response/ResponseServlet.java
+++ 01_Servlet/src/main/java/ex04_response/ResponseServlet.java
@@ -13,10 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ResponseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ResponseServlet() {
-        super();
-    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
