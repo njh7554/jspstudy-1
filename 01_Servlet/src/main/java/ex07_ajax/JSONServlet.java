4babbf3caf51d6c1a70bbbe7e7e17b3d67d28d52
gdu-dev <minkt78@naver.com>
2023-03-23 09:48:11 +0900

    2023-03-23

--- 01_Servlet/src/main/java/ex07_ajax/JSONServlet.java
+++ 01_Servlet/src/main/java/ex07_ajax/JSONServlet.java
@@ -26,34 +26,51 @@ public class JSONServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		// 요청 인코딩
-		request.setCharacterEncoding("UTF-8");
-		
-		// 요청 파라미터
-		String name = request.getParameter("name");
-		String strAge = request.getParameter("age");
-		int age = 0;
-		if(strAge != null && strAge.isEmpty() == false) {
-			age = Integer.parseInt(strAge);
+		try {
+			
+			// 요청 인코딩
+			request.setCharacterEncoding("UTF-8");
+			
+			// 요청 파라미터
+			String name = request.getParameter("name");
+			String strAge = request.getParameter("age");
+			int age = 0;
+			if(strAge != null && strAge.isEmpty() == false) {
+				age = Integer.parseInt(strAge);
+			}
+			
+			// 나이 예외 처리
+			if(age < 0 || age > 100) {
+				throw new AgeHandleException(age + "살은 잘못된 나이입니다", 600);
+			}
+			
+			// 응답할 JSON 데이터
+			JSONObject obj = new JSONObject();
+			obj.put("name", name);
+			obj.put("age", age);
+			// System.out.println(obj.toString());  // {"name":"마돈나", "age":50}
+			
+			// 응답 데이터 타입
+			response.setContentType("application/json; charset=UTF-8");
+			
+			// 출력 스트림 생성
+			PrintWriter out = response.getWriter();
+			
+			// 출력
+			String resData = obj.toString();
+			out.println(resData);  // 텍스트 형식으로 된 JSON 데이터를 응답한다.
+			out.flush();
+			out.close();
+			
+		} catch(AgeHandleException e) {
+			
+			response.setContentType("text/plain; charset=UTF-8");
+			
+			response.setStatus(e.getErrorCode());
+			
+			response.getWriter().println(e.getMessage());
+			
 		}
-		
-		// 응답할 JSON 데이터
-		JSONObject obj = new JSONObject();
-		obj.put("name", name);
-		obj.put("age", age);
-		// System.out.println(obj.toString());  // {"name":"마돈나", "age":50}
-		
-		// 응답 데이터 타입
-		response.setContentType("application/json; charset=UTF-8");
-		
-		// 출력 스트림 생성
-		PrintWriter out = response.getWriter();
-		
-		// 출력
-		String resData = obj.toString();
-		out.println(resData);  // 텍스트 형식으로 된 JSON 데이터를 응답한다.
-		out.flush();
-		out.close();
 	
 	}
 
